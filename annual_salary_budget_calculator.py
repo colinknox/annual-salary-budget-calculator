5baafe3d83b866122394abb420c755e6e9ce54dc
Ninny <neninny@proton.me>
2026-07-05 00:59:11 -0400

    Wrote up pseudocode for project.

---
+++ annual_salary_budget_calculator.py
@@ -0,0 +1,10 @@
+# Loop
+    # Ask user for their annual salary
+    # If invalid value
+        # Print "Invalid value!"
+    # Print entire budget
+    # Ask user if they want to restart or end program (y/n)
+    # If y
+        # Restart program
+    # Else
+        # Terminate
