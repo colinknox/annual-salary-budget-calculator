afcb1e55f4c8c152e59a423770420681d77ef717
Ninny <neninny@proton.me>
2026-07-05 01:20:08 -0400

    User will now see their monthly and annual budget.

--- annual_salary_budget_calculator.py
+++ annual_salary_budget_calculator.py
@@ -3,7 +3,19 @@
 try:
     user_annual_salary = int(input("Hello! Please input your annual salary: "))
     
-    print(f"Your annual salary is ${user_annual_salary}!")
+    print(f"\nYour annual salary is ${user_annual_salary}!\n")
+
+    if user_annual_salary >= 100000:
+        print("***** ANNUALLY BREAK DOWN *****")
+        print(f"  NEEDS (50%): {user_annual_salary * 0.5}")
+        print(f"  WANTS (30%): {user_annual_salary * 0.3}")
+        print(f"SAVINGS (20%): {user_annual_salary * 0.2}")
+
+        print("\n***** MONTHLY BREAK DOWN *****")
+        print(f"  NEEDS (50%): {user_annual_salary * 0.5 / 12}")
+        print(f"  WANTS (30%): {user_annual_salary * 0.3 / 12}")
+        print(f"SAVINGS (20%): {user_annual_salary * 0.2 / 12}")
+
 except ValueError:
     print("Invalid value! Please input a number for your salary.")
 
